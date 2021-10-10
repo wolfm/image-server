904a56b7369fcf5ab2743fe7e6abb93d39919017
Michael Wolf <mjwolf@umich.edu>
2021-10-10 11:09:55 -0400

    Initialize server state with existing photos

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, redirect
-import os.path
+import os
 from flask.helpers import send_file
 from werkzeug.utils import secure_filename
 from PIL import Image
@@ -7,8 +7,22 @@ from PIL import Image
 VALID_EXTENSIONS = set(["jpg", "jpeg", "png", "gif", "webp"])
 UPLOAD_FOLDER = 'uploads'
 
+
 app = Flask(__name__, static_url_path='', static_folder='../client/build')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+photos = []
+
+@app.before_first_request
+def initPhotos():
+    # For each file
+    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+
+        # Validate extension
+        if not valid_file(filename): continue
+
+        add_photo_to_state(filename)
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -31,16 +45,7 @@ def upload():
 
                     file.save(filepath)
 
-                    # Get image properties
-                    img = Image.open(filepath)
-                    width, height = img.size
-
-                    # Update server state
-                    photos.append({
-                        'src': os.path.join('photos', filename), #TODO make photos part of config, not hardcoded
-                        'width': width, # TODO calculate
-                        'height': height
-                    })
+                    add_photo_to_state(filename)
                 else:
                     return_code = 415 # Unsupported media type
 
@@ -67,100 +72,28 @@ def getPhoto(filename):
         return f"File at '{path}' not found!", 404
     
     return send_file(path)
-        
+
+
+### HELPER FUNCTIONS ###
+
+
+# Validate file against specified extensions
 def valid_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in VALID_EXTENSIONS
 
-photos = []
-#     {
-#       'src': "http://localhost:5000/photos/grapes.jpg",
-#       'width': 4,
-#       'height': 3
-#     },
-#     {
-#       'src': "http://localhost:5000/photos/abstract_crop.jpg",
-#       'width': 1,
-#       'height': 1
-#     },
-#     {
-#       'src': "https://source.unsplash.com/Dm-qxdynoEc/800x799",
-#       'width': 1,
-#       'height': 1
-#     },
-#     {
-#       'src': "https://source.unsplash.com/qDkso9nvCg0/600x799",
-#       'width': 3,
-#       'height': 4
-#     },
-#     {
-#       'src': "https://source.unsplash.com/iecJiKe_RNg/600x799",
-#       'width': 3,
-#       'height': 4
-#     },
-#     {
-#       'src': "https://source.unsplash.com/epcsn8Ed8kY/600x799",
-#       'width': 3,
-#       'height': 4
-#     },
-#     {
-#       'src': "https://source.unsplash.com/NQSWvyVRIJk/800x599",
-#       'width': 4,
-#       'height': 3
-#     },
-#     {
-#       'src': "https://source.unsplash.com/zh7GEuORbUw/600x799",
-#       'width': 3,
-#       'height': 4
-#     },
-#     {
-#       'src': "https://source.unsplash.com/PpOHJezOalU/800x599",
-#       'width': 4,
-#       'height': 3
-#     },
-#     {
-#       'src': "https://source.unsplash.com/I1ASdgphUH4/800x599",
-#       'width': 4,
-#       'height': 3
-#     },
-#     {
-#       'src': "https://source.unsplash.com/XiDA78wAZVw/600x799",
-#       'width': 3,
-#       'height': 4
-#     },
-#     {
-#       'src': "https://source.unsplash.com/x8xJpClTvR0/800x599",
-#       'width': 4,
-#       'height': 3
-#     },
-#     {
-#       'src': "https://source.unsplash.com/u9cG4cuJ6bU/4927x1000",
-#       'width': 4927,
-#       'height': 1000
-#     },
-#     {
-#       'src': "https://source.unsplash.com/qGQNmBE7mYw/800x599",
-#       'width': 4,
-#       'height': 3
-#     },
-#     {
-#       'src': "https://source.unsplash.com/NuO6iTBkHxE/800x599",
-#       'width': 4,
-#       'height': 3
-#     },
-#     {
-#       'src': "https://source.unsplash.com/pF1ug8ysTtY/600x400",
-#       'width': 4,
-#       'height': 3
-#     },
-#     {
-#       'src': "https://source.unsplash.com/A-fubu9QJxE/800x533",
-#       'width': 4,
-#       'height': 3
-#     },
-#     {
-#       'src': "https://source.unsplash.com/5P91SF0zNsI/740x494",
-#       'width': 4,
-#       'height': 3
-#     }
-#   ]
+
+def add_photo_to_state(filename):
+
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    # Get image properties
+    img = Image.open(filepath)
+    width, height = img.size
+
+    # Update server state
+    photos.append({
+        'src': os.path.join('photos', filename), #TODO make photos part of config, not hardcoded
+        'width': width,
+        'height': height
+    })
